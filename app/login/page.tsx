464b2137cfa10dd707c9184c0f57b75691b79eb8
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-05-07 03:31:52 +0800

    feat: add account creation

---
+++ app/login/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoginAuth from "../components/AuthProvider/LoginAuth";
+
+const Login = () => {
+  return <LoginAuth />;
+};
+
+export default Login;
---
+++ app/register/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import RegisterAuth from "../components/AuthProvider/RegisterAuth";
+
+const Register = () => {
+  return <RegisterAuth />;
+};
+
+export default Register;
