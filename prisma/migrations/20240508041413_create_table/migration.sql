fa5d93d36d5791ff0f024a39e9a736ef78daff5d
ANTHONY JOHN APARICIO <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-05-08 13:05:03 +0800

    build: change migrations

--- prisma/migrations/20240508041413_create_table/migration.sql
+++ prisma/migrations/20240508041413_create_table/migration.sql
@@ -3,8 +3,10 @@ CREATE TABLE "Users" (
     "id" TEXT NOT NULL,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
+    "username" VARCHAR(14) NOT NULL,
     "userType" TEXT NOT NULL,
-    "bio" TEXT NOT NULL DEFAULT 'User has no info',
+    "location" TEXT NOT NULL DEFAULT 'None',
+    "bio" VARCHAR(255) NOT NULL DEFAULT 'User has no info',
     "profilePicture" TEXT NOT NULL DEFAULT 'defaultPhoto.png',
     "isAdmin" BOOLEAN NOT NULL DEFAULT false,
 
@@ -13,3 +15,6 @@ CREATE TABLE "Users" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Users_id_key" ON "Users"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_username_key" ON "Users"("username");
