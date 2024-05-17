4217b144d8c038cc870ae645cd67224a8c8aca49
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-05-18 00:15:41 +0800

    test: setup testing libraries

---
+++ jest.config.ts
@@ -0,0 +1,29 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./"
+});
+
+const config: Config = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+
+  testEnvironment: "jsdom",
+  testRegex: "/__tests__/.*\\.test\\.tsx$",
+  transform: {
+    "^.+\\.tsx?$": "@swc/jest"
+  },
+  verbose: true,
+  forceExit: true,
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1"
+  }
+};
+
+export default createJestConfig(config);
