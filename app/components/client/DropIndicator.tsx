4bdaca0c3d58801c817cc3a056d3b50d860f05c1
Jezerwel <jezerwel.grino-22@cpu.edu.ph>
2024-05-18 18:24:52 +0800

    feat(frontend): add kanban board indicator component

---
+++ app/components/client/DropIndicator.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { DropIndicatorProps } from "@/app/utils/KanbanProps";
+
+const DropIndicator: React.FC<DropIndicatorProps> = ({ beforeId, column }) => {
+  return (
+    <div
+      data-before={beforeId || "-1"}
+      data-column={column}
+      className="my-0.5 h-0.5 w-full bg-violet-400 opacity-0"
+    />
+  );
+};
+
+export default DropIndicator;
