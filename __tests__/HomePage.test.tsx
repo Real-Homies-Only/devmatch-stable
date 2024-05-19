8fed3b9c6bf08b70b4a68861932d66a4776f433c
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-05-20 00:55:55 +0800

    test: add test for home page

--- __tests__/HomePage.test.tsx
+++ __tests__/HomePage.test.tsx
@@ -1,6 +1,8 @@
 import { screen, render } from "@testing-library/react";
 import { it, describe } from "@jest/globals";
+
 import GetStarted from "@/app/components/client/GetStarted";
+import Hero from "@/app/components/client/Hero";
 
 // Mock the useRouter function outside the test cases
 let mockPush: any;
@@ -26,12 +28,26 @@ jest.mock("next/navigation", () => ({
   }))
 }));
 
-describe("Test", () => {
-  it("have the get started text", () => {
+describe("Home Page", () => {
+  it("has the get started text", () => {
     render(<GetStarted />);
     const getStartedButton = screen.getByRole("button", {
       name: /Get Started/i
     });
     expect(getStartedButton).toBeInTheDocument();
+  });
+
+  it("shows the hero section", () => {
+    render(<Hero />);
+    const heroTitle = screen.getByText(/Match. Create. Repeat./i);
+    expect(heroTitle).toBeInTheDocument();
+  });
+
+  it("shows the hero section subtitle", () => {
+    render(<Hero />);
+    const heroSubtitle = screen.getByText(
+      /DevMatch is a platform that simplifies the process of finding and hiring expert freelance developers and designers for projects of all sizes/i
+    );
+    expect(heroSubtitle).toBeInTheDocument();
   });
 });
