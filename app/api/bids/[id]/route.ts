8ef2e01f996df2263e5bac6d857f632666e29010
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-05-23 14:28:13 +0800

    fix(bids): add a projectId parameter as users can only bid one time on all projects

--- app/api/bids/[id]/route.ts
+++ app/api/bids/[id]/route.ts
@@ -15,6 +15,7 @@ export async function POST(
 
       const existingBid = await prisma.bids.findFirst({
         where: {
+          projectId: idString,
           userId: userId
         }
       });
